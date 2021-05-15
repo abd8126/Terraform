4bda6dff602b5383c252e02cede2952bdf96b817
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2021-05-15 14:02:04 +0530

    Update code

--- Terraform LW/first.tf
+++ Terraform LW/first.tf
@@ -1,5 +1,15 @@
 provider "aws" {
-  region = "ap-south-1a"
-  access_key = ""
-  privte_key = ""
+  region = "ap-south-1"
+  profile = "default"
+}
+resource "aws_instance" "os1" {
+  ami = "ami-010aff33ed5991201"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "terraform"
+  }
+}
+
+output "resutt" {
+  value = "aws_instance.os1.availability_zone"
 }
