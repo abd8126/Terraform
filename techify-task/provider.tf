7ce7b38a065abeb8605140962b4b677a3ab460bc
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2021-09-07 01:31:03 +0530

    Create provider.tf

---
+++ techify-task/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 3.0"
+        }
+    }
+}
+provider "aws" {
+    access_key = "AKIAZJLI7HSFPUQFVX65"
+    secret_key = "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"
+    region = "ap-south-1"
+}
