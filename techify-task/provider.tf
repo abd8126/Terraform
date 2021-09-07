f963aa5bbaa4f89d62ab992308ad92f64ad6051d
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2021-09-07 18:35:22 +0530

    Update provider.tf

--- techify-task/provider.tf
+++ techify-task/provider.tf
@@ -7,7 +7,7 @@ terraform {
     }
 }
 provider "aws" {
-    access_key = "AKIAVTEK4MX4FG44I3MM"
-    secret_key = "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"
+    access_key = "AKIAVTEK4MX4NNHZYDFU"
+    secret_key = "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"
     region = "ap-south-1"
 }
